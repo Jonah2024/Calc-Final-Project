63e59c5db69e0a9459bbf162eb9dc81255f3dec4
Jonah2024 <118486455+Jonah2024@users.noreply.github.com>
2023-05-18 13:06:49 -0400

    Update ColorPanel.java

--- ColorPanel.java
+++ ColorPanel.java
@@ -5,7 +5,13 @@ import java.awt.*;
 
 public class ColorPanel extends JPanel{
 
+    double[] xCoordinates = new double[10000];
+    double[] yCoordinates = new double[10000];
+
     public void paintComponent(Graphics g) {
+
+        SimpleGraph graph = new SimpleGraph();
+
         super.paintComponent(g);
 
         g.setColor(Color.decode("#EAEAEA"));
@@ -15,8 +21,26 @@ public class ColorPanel extends JPanel{
         g.fillRect(0, 500, 1000, 3);
         g.fillRect(1000, 0, 1, 1000);
 
+        g.fillOval(600, 400, 5, 5);
+        g.fillOval(700, 300, 5, 5);
+        g.fillOval(800, 200, 5, 5);
+        g.fillOval(900, 100, 5, 5);
+
+        g.setColor(Color.RED);
+        for (int i = 0; i < xCoordinates.length; i ++){
+            if(i % 100 == 0){
+                 //System.out.println("(" + (int) (xCoordinates[i]) + ", " + (int) (-1 * yCoordinates[i]) + ")");
+            }
+           
+            g.fillOval((int) (xCoordinates[i] * (100) + 500), (int) (-1 * yCoordinates[i] * 100 + 500), 3, 3);
+        }
+
     }
 
+    public void createCoordinates(double x, double y, int place){
+        xCoordinates[place] = x;
+        yCoordinates[place] = y;
+    }
     
 
 
